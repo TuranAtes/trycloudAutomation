d470fcdd027ec8110f44201aeb78745c1b15d872
AnastasiaSmorodin <anastasiasmorodin@gmail.com>
2022-04-03 13:42:13 -0500

    initial commit us11

--- src/test/java/com/trycloud/runner/cukesRunner.java
+++ src/test/java/com/trycloud/runner/cukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/trycloud/step_definitions",
-        dryRun = false,
+        dryRun = true,
         tags = "@11",
         publish = false
 )
