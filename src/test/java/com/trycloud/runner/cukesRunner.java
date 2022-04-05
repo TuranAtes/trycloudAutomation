08d880e079c7eec91663b30d3c1f5e4e1d022b56
Turan Ates <cezam505@gmail.com>
2022-04-05 10:27:02 -0400

    Update

--- src/test/java/com/trycloud/runner/cukesRunner.java
+++ src/test/java/com/trycloud/runner/cukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/trycloud/step_definitions",
         dryRun = false,
-        tags = "@us13",
+        tags = "",
         publish = false
 )
 public class cukesRunner {
